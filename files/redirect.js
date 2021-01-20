a1dc7ca59e509f989c36251c74b1bc3d59095361
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-20 00:43:53 -0300

    preparing redirect

--- files/redirect.js
+++ files/redirect.js
@@ -36,93 +36,95 @@ module.exports = async function (req, res, cfg, existSession) {
 
                 // Check Session
                 if (typeof tinyCfg.csrfToken !== "string" || tinyCfg.csrfToken.length < 1 || (typeof req.query.state.csrfToken === "string" && req.query.state.csrfToken === tinyCfg.csrfToken)) {
+
+                    // Prepare Resolve Data
+                    const resolveData = { newSession: false };
+
+                    // Exist Session
                     if (!existSession) {
 
-                        if (!req.query.code) throw new Error('NoCodeProvided');
+                        // Check New Session
+                        resolveData.newSession = true;
 
-                        // Discord Token
-                        const getToken = require('./api/getToken');
+                        if (typeof req.query.code === "string" && req.query.code.length > 0) {
 
-                        const code = req.query.code;
-                        getToken({
-                            client_id: tinyCfg.discordID,
-                            client_secret: tinyCfg.discordSecret,
-                            code: code,
-                            redirect_uri: tinyCfg.redirect,
-                            scope: tinyCfg.discordScope.join(' ')
-                        })
+                            // Discord Token
+                            const getToken = require('./api/getToken');
 
-                            // Success
-                            .then(json => {
-
-                                // Check Token
-                                if (json.data && (typeof json.data.access_token === "string" || typeof json.data.access_token === "number")) {
-
-                                    // Get JSON
-                                    json = json.data;
-
-
-
-                                    // Discord Token
-                                    const getUser = require('./api/getUser');
-
-                                    if (tinyCfg.getUser) {
-
-                                        // Get User
-                                        getUser(json.access_token)
-
-                                            // Get User
-                                            .then(dsUser => {
-
-                                                // User Verified
-                                                if ((objType(dsUser.data, 'object') && dsUser.data.verified) || !tinyCfg.needVerification) {
-
-                                                    // Get Data
-                                                    dsUser = dsUser.data;
-
-                                                    // Redirect
-                                                    if (typeof callback !== "function") {
-
-                                                        req.session.access_token = json.access_token;
-                                                        if (typeof req.query.state.redirect !== "string") {
-                                                            return res.redirect('/');
-                                                        } else {
-                                                            return res.redirect('/' + req.query.state.redirect);
-                                                        }
-
-                                                    }
-
-                                                    // Custom Redirect
-                                                    else {
-                                                        return callback(json, dsUser, json.access_token);
-                                                    }
-
-                                                }
-
-                                                // Ops!
-                                                else {
-                                                    reject({ code: 401, message: 'Discord account need to be verified.' });
-                                                }
-
-                                            })
-
-                                            // Fail
-                                            .catch(err => {
-                                                reject({ code: err.response.status, message: err.message });
-                                            });
-
-                                    }
-
-                                } else {
-                                    reject({ code: 500, message: 'Incorrect User Data!' });
-                                }
-
+                            const code = req.query.code;
+                            getToken({
+                                client_id: tinyCfg.discordID,
+                                client_secret: tinyCfg.discordSecret,
+                                code: code,
+                                redirect_uri: tinyCfg.redirect,
+                                scope: tinyCfg.discordScope.join(' ')
                             })
 
-                            // Fail
-                            .catch(err => {
-                                reject({ code: err.response.status, message: err.message });
-                            });
+                                // Success
+                                .then(json => {
+
+                                    // Check Token
+                                    if (objType(json, 'object') && objType(json.data, 'object') && (typeof json.data.access_token === "string" || typeof json.data.access_token === "number")) {
+
+                                        // Token
+                                        resolveData.token = json;
+
+                                        // Get User Data
+                                        if (tinyCfg.getUser) {
+
+                                            // Discord Token
+                                            const getUser = require('./api/getUser');
+
+                                            // Get User
+                                            getUser(json.data.access_token)
+
+                                                // Get User
+                                                .then(dsUser => {
+
+                                                    // User Verified
+                                                    if ((objType(dsUser.data, 'object') && dsUser.data.verified) || !tinyCfg.needVerification) {
+
+                                                        // User Data
+                                                        resolveData.user = dsUser;
+                                                        resolve({});
+
+                                                    }
+
+                                                    // Ops!
+                                                    else {
+                                                        reject({ code: 401, message: 'Discord account need to be verified.' });
+                                                    }
+
+                                                })
+
+                                                // Fail
+                                                .catch(err => {
+                                                    reject({ code: err.response.status, message: err.message });
+                                                });
+
+                                        }
+
+                                        // Nope
+                                        else {
+
+
+
+                                        }
+
+                                    } else {
+                                        reject({ code: 500, message: 'Incorrect User Data!' });
+                                    }
+
+                                })
+
+                                // Fail
+                                .catch(err => {
+                                    reject({ code: err.response.status, message: err.message });
+                                });
+
+                        } else {
+                            reject({ code: 401, message: 'Incorrect Discord Code!' });
+                        }
 
                     } else {
                         resolve({ newSession: false });
