38b2ce3cf22f71818e4ccc834fdf15aa0010cbac
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-20 00:52:00 -0300

    preparing test code

--- test/index.js
+++ test/index.js
@@ -2,3 +2,14 @@
 const discordAuth = require('../index');
 const express = require('express');
 const app = express();
+
+// Port
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Discord Code Test is being executed in http://localhost:${port}`);
+})
