1f8f2e6028d1f297e99bf72da5c4a2ca0cf28140
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-20 00:52:36 -0300

    server listen test

--- test/index.js
+++ test/index.js
@@ -6,10 +6,12 @@ const app = express();
 // Port
 const port = 3000
 
+// Path
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+// Listen the Server
 app.listen(port, () => {
   console.log(`Discord Code Test is being executed in http://localhost:${port}`);
 })
