8a53a2cc0b16448bc05e6f31ecb02896f35d3c2c
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-19 21:53:30 -0300

    logout update

--- files/logout.js
+++ files/logout.js
@@ -1,43 +1,52 @@
 
 module.exports = async function (req, res, cfg, existSession) {
+    return new Promise(function (resolve, reject) {
 
-    // Prepare Modules
-    const objType = require('@tinypudding/puddy-lib/get/objType');
-    const http_status = require('@tinypudding/puddy-lib/http/HTTP-1.0');
-    const _ = require('lodash');
+        // Prepare Modules
+        const objType = require('@tinypudding/puddy-lib/get/objType');
+        const _ = require('lodash');
 
-    // Create Settings
-    const tinyQuery = _.defaultsDeep({}, cfg.query, {
-        csrfToken: 'csrfToken',
-        redirect: 'redirect'
-    });
+        // Create Settings
+        const tinyQuery = _.defaultsDeep({}, cfg.query, {
+            csrfToken: 'csrfToken',
+            redirect: 'redirect'
+        });
 
-    // Prepare Final Redirect
-    let finalRedirect = '/';
+        // Prepare Final Redirect
+        let finalRedirect = '/';
 
-    // Redirect
-    if (objType(req.query, 'object') && req.query.redirect) {
+        // Redirect
+        if (objType(req.query, 'object') && req.query[tinyQuery.redirect]) {
 
-        if (req.query.redirect.startsWith('/')) {
-            req.query.redirect = req.query.redirect.substring(1);
+            if (req.query[tinyQuery.redirect].startsWith('/')) {
+                finalRedirect = req.query[tinyQuery.redirect].substring(1);
+            } else {
+                finalRedirect = req.query[tinyQuery.redirect];
+            }
+
         }
 
-    } else {
-        req.query.redirect = '';
-    }
+        // Exist Session
+        if (existSession) {
 
-    // Exist Session
-    if (existSession) {
+            // Get API HTTP and Revoke the Token
+            const discord_api = require('./api');
+            discord_api.revokeToken(cfg.access_token).then(() => {
+                resolve(function () { res.redirect(finalRedirect); });
+                return;
+            }).catch(err => {
+                reject(err);
+            });
 
-        // Get API HTTP and Revoke the Token
-        const discord_api = require('./api');
-        await discord_api.revokeToken(cfg.access_token);
+        }
 
-        req.session = null;
+        // Nope
+        else {
+            resolve(function () { res.redirect(finalRedirect); });
+        }
 
-    }
+        // Action Complete
+        return;
 
-    // Action Complete
-    return res.redirect(finalRedirect);
-
+    });
 }
