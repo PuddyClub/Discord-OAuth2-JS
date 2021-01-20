b74c1aaa3c80ada5129e4f14a3f2271469e436a5
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-20 01:01:42 -0300

    prepare test

--- test/index.js
+++ test/index.js
@@ -3,15 +3,43 @@ const discordAuth = require('../index');
 const express = require('express');
 const app = express();
 
-// Port
-const port = 3000
+// Prepare Body Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const bodyParseN = bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+});
 
-// Path
+// Add Helmet
+const helmet = require('helmet');
+app.use(helmet());
+
+// Prepare Cooke Session
+const cookieSession = require('cookie-session');
+const tinySession = cookieSession({
+    keys: ['0000000000', '11111111111']
+});
+
+app.use(tinySession);
+
+// Port
+const port = 3000;
+
+// Discord Login
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+    return discordAuth.login(req, res, cfg, existSession);
+
+});
+
+// Others
+app.post('*', bodyParseN, (req, res) => {
+    return startPage(botOwner, bot, tinyCfg, res, req, db, isDebug, (defaultPage, page, ip, moment) => {
+        return domainSelector(req, res, defaultPage, page, ip, moment);
+    });
 });
 
 // Listen the Server
 app.listen(port, () => {
-  console.log(`Discord Code Test is being executed in http://localhost:${port}`);
+    console.log(`Discord Code Test is being executed in http://localhost:${port}`);
 })
