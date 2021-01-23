a842e53cde4c3ec7593dd7ff30a0b0136982063e
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-22 21:55:15 -0300

    preparing more thing to template

--- template/cookie-session.js
+++ template/cookie-session.js
@@ -3,35 +3,38 @@ module.exports = function (cfg, app) {
     // OBJ Type
     const objType = require('@tinypudding/puddy-lib/get/objType');
 
-    // Modules
-    const _ = require('lodash');
+    // Config
+    if (objType(cfg, 'object')) {
 
-    // Create Settings
-    const tinyCfg = _.defaultsDeep({}, cfg, {
-       
-        // Body Parser
-        bodyParser: {
+        // Modules
+        const _ = require('lodash');
+
+        // Create Settings
+        const bodyParser = _.defaultsDeep({}, cfg.bodyParser, {
             module: null,
             used: false
+        });
+
+        // Test Modules Prepare
+        const discordAuth = require('../index');
+        const getSessionFromCookie = require('../get/cookie-session');
+        let bodyParse = null;
+
+        // Prepare Body Parser
+        if (bodyParser.used) {
+            if (bodyParser.module) {
+
+                bodyParser.module = require('body-parser');
+                bodyParse = bodyParser.module.urlencoded({     // to support URL-encoded bodies
+                    extended: true
+                });
+
+            }
         }
 
-    });
-
-    // Test Modules Prepare
-    const discordAuth = require('../index');
-    const getSessionFromCookie = require('../get/cookie-session');
-    let bodyParse = null;
-
-    // Prepare Body Parser
-    if(tinyCfg.bodyParser.used) {
-        if(tinyCfg.bodyParser.module) {
-        
-            tinyCfg.bodyParser.module = require('body-parser');
-            bodyParse = tinyCfg.bodyParser.module.urlencoded({     // to support URL-encoded bodies
-                extended: true
-            });
-
-        }
     }
+
+    // Complete
+    return;
 
 };
