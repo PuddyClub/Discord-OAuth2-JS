d6e419d0649a04968ee00b0f1d972365625f7069
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-23 19:24:56 -0300

    template update

--- test/template/cookie-session.js
+++ test/template/cookie-session.js
@@ -23,10 +23,45 @@ tinyAuth.discordScope = ["identify"];
 // Result
 discordAuth(app, { auth: tinyAuth });
 
+// Homepage
+app.get('/', (req, res) => {
+    res.send('Tiny Homepage :3');
+});
+
+// User Page
+app.get('/user', (req, res) => {
+
+    // Result
+    if (typeof req.session[sessionVars.access_token] === "string") {
+        discordAuth.api.getUser(req.session[sessionVars.access_token]).then(result => {
+
+            // Complete
+            res.json(result);
+            return;
+
+        }).catch(err => {
+
+            // Complete
+            res.json(err);
+            return;
+
+        });
+    }
+
+    // Nope
+    else {
+        res.send('No Account Detect');
+    }
+
+});
+
+
 // Listen the Server
 app.listen(port, () => {
 
+    console.log(`Homepage: http://localhost:${port}/`);
+    console.log(`User Page: http://localhost:${port}/user`);
     console.log(`Login: http://localhost:${port}/login`);
-    console.log(`Login: http://localhost:${port}/logout`);
+    console.log(`Logout: http://localhost:${port}/logout`);
 
 });
