7abee5928ed0f095e0f55375c160f1dc3324ef5b
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-22 22:32:47 -0300

    preparing values from the template

--- template/cookie-session.js
+++ template/cookie-session.js
@@ -10,27 +10,183 @@ module.exports = function (cfg, app) {
         const _ = require('lodash');
 
         // Create Settings
-        const bodyParser = _.defaultsDeep({}, cfg.bodyParser, {
-            module: null,
-            used: false
+        const tinyCfg = _.defaultsDeep({}, cfg.cfg, {
+            errorCallback: function (err) {
+                return res.json(err);
+            }
         });
 
         // Test Modules Prepare
         const discordAuth = require('../index');
         const getSessionFromCookie = require('../get/cookie-session');
-        let bodyParse = null;
 
-        // Prepare Body Parser
-        if (bodyParser.used) {
-            if (bodyParser.module) {
+        // Auth Config
+        const tinyAuth = _.defaultsDeep({}, cfg.auth, {
+            redirect: "",
+            client_id: "",
+            client_secret: "",
+            bot_token: "",
+            get_user: true,
+            discordScope: []
+        });
 
-                bodyParser.module = require('body-parser');
-                bodyParse = bodyParser.module.urlencoded({     // to support URL-encoded bodies
-                    extended: true
-                });
+        // Session Vars
+        const sessionVars = _.defaultsDeep({}, cfg.vars, {
+            access_token: 'access_token',
+            expires_in: 'expires_in',
+            refresh_token: 'refresh_token',
+            token_type: 'token_type',
+            scope: 'scope'
+        });
 
-            }
-        }
+        // Crypto Values
+        const tinyCrypto = _.defaultsDeep({}, cfg.crypto, {
+            algorithm: 'aes-256-ctr',
+            password: 'tinypudding'
+        });
+
+        // Crypto Values
+        const tinyURLPath = _.defaultsDeep({}, cfg.url, {
+            login: '/login',
+            webhook: '/webhook',
+            logout: '/logout',
+            redirect: '/redirect'
+        });
+
+        // Login
+        app.get(tinyURLPath.login, (req, res) => {
+
+            // Result
+            discordAuth.login(req, res,
+                {
+
+                    // Error
+                    errorCallback: tinyCfg.errorCallback,
+
+                    // Crypto
+                    crypto: tinyCrypto,
+
+                    // Auth
+                    auth: tinyAuth,
+
+                    // Type
+                    type: 'login',
+
+                    // Query
+                    query: { redirect: 'redirect' },
+
+                    // State
+                    state: {
+                        csrfToken: req.session[sessionVars.access_token],
+                        redirect: ''
+                    }
+
+                }, (getSessionFromCookie(req, sessionVars.access_token)),
+            );
+
+            // Complete
+            return;
+
+        });
+
+        // Logout
+        app.get(tinyURLPath.logout, (req, res) => {
+
+            // Result
+            discordAuth.logout(req, res, req.session,
+                {
+
+                    // Query
+                    query: {
+                        csrfToken: 'csrfToken',
+                        redirect: 'redirect'
+                    },
+
+                    // Auth
+                    auth: tinyAuth,
+
+                    // Access Token
+                    access_token: req.session[sessionVars.access_token]
+
+                }, (getSessionFromCookie(req, sessionVars.access_token)),
+            ).then(result => {
+
+                // Complete
+                req.session = null;
+                res.json(result);
+                return;
+
+            }).catch(err => {
+
+                // Complete
+                res.json(err);
+                return;
+
+            });
+
+            // Complete
+            return;
+
+        });
+
+        // Redirect
+        app.get(tinyURLPath.redirect, bodyParseN, (req, res) => {
+
+            // Result
+            discordAuth.redirect(req,
+                {
+
+                    // Auth
+                    auth: tinyAuth,
+
+                    // Crypto
+                    crypto: tinyCrypto,
+
+                    // Query
+                    query: { redirect: 'redirect' },
+
+                    // State
+                    state: {
+                        csrfToken: '',
+                        redirect: ''
+                    }
+
+                }, (getSessionFromCookie(req, sessionVars.access_token)),
+            ).then(result => {
+
+                // New Session Result
+                if (result.state.type === "login" && result.newSession) {
+                    req.session[sessionVars.access_token] = result.tokenRequest.access_token;
+                    req.session[sessionVars.expires_in] = result.tokenRequest.expires_in;
+                    req.session[sessionVars.refresh_token] = result.tokenRequest.refresh_token;
+                    req.session[sessionVars.token_type] = result.tokenRequest.token_type;
+                    req.session[sessionVars.scope] = result.tokenRequest.scope;
+                    res.json(result);
+                }
+
+                // Webhook Result
+                else if (result.state.type === "webhook") {
+                    res.json(result);
+                }
+
+                // Nothing
+                else {
+                    res.redirect('/');
+                }
+                return;
+
+            }).catch(err => {
+
+                // Complete
+                res.json(err);
+                return;
+
+            });
+
+            // Complete
+            return;
+
+        });
 
     }
 
