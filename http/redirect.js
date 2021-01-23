fc974a51b6cd6a68f9180169ab8016ca5e023088
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-23 20:40:51 -0300

    redirect fixed

--- http/redirect.js
+++ http/redirect.js
@@ -50,14 +50,14 @@ module.exports = async function (req, cfg, existSession) {
                         // Check Session
                         if (typeof tinyCfg.csrfToken !== "string" || tinyCfg.csrfToken.length < 1 || (typeof req.query.state.csrfToken === "string" && req.query.state.csrfToken === tinyCfg.csrfToken)) {
 
-                            // Final Redirect
-                            let final_redirect = '/';
-                            if(typeof req.query.state.redirect === "string") {
-                                final_redirect += req.query.state.redirect;
-                            }
-
                             // Prepare Resolve Data
                             const resolveData = { newSession: false, state: req.query.state };
+
+                            // Final Redirect
+                            resolveData.redirect = '/';
+                            if (typeof req.query.state.redirect === "string") {
+                                resolveData.redirect += req.query.state.redirect;
+                            }
 
                             // Exist Session
                             if (!existSession || req.query.state.type === "webhook") {
