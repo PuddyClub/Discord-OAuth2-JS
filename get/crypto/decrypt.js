b359b021d2ce29a630a03a6ee06685d1aa3f8866
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-22 23:36:51 -0300

    key

--- get/crypto/decrypt.js
+++ get/crypto/decrypt.js
@@ -3,7 +3,7 @@ module.exports = function (tinyCrypto, text) {
     let textParts = text.split(':');
     let iv = Buffer.from(textParts.shift(), 'hex');
     let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    let decipher = crypto.createDecipheriv(tinyCrypto.algorithm, Buffer.from(tinyCrypto.password), iv);
+    let decipher = crypto.createDecipheriv(tinyCrypto.algorithm, Buffer.from(tinyCrypto.key), iv);
     let decrypted = decipher.update(encryptedText);
    
     decrypted = Buffer.concat([decrypted, decipher.final()]);
--- get/crypto/encrypt.js
+++ get/crypto/encrypt.js
@@ -1,7 +1,7 @@
 module.exports = function (tinyCrypto, text) {
 
     let iv = crypto.randomBytes(tinyCrypto.IV_LENGTH);
-    let cipher = crypto.createCipheriv(tinyCrypto.algorithm, Buffer.from(tinyCrypto.password), iv);
+    let cipher = crypto.createCipheriv(tinyCrypto.algorithm, Buffer.from(tinyCrypto.key), iv);
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     return iv.toString('hex') + ':' + encrypted.toString('hex');
