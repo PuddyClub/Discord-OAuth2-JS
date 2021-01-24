2d2746028acf83c6db9ee8b73bc6e805cde5ebb4
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-24 02:50:42 -0300

    prepare firebase

--- test/template/cookie-session/firebase.js
+++ test/template/cookie-session/firebase.js
@@ -1,4 +1,19 @@
+// Prepare Firebase
+const tinyCfg = require('../../firebase_config.json');
+const firebase = require('@tinypudding/firebase-lib');
 
+// Get Credentials
+tinyCfg.firebase.credential = admin.credential.cert(require('../../firebase.json'));
 
 // Start Firebase
-require('./index')('firebase');
+if (tinyCfg.firebase) {
+    firebase.start(require('firebase-admin'), tinyCfg.options, tinyCfg.firebase);
+} else {
+    firebase.start(require('firebase-admin'), tinyCfg.options, tinyCfg.firebase);
+}
+
+// App
+const app = firebase.get(tinyCfg.options.id);
+
+// Start Firebase
+require('./index')('firebase', app);
