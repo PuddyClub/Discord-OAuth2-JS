f706984ffa56f3b6f3b89d8f824623747848ad82
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-26 10:59:25 -0300

    login and logout template

--- client/login.js
+++ client/login.js
@@ -1,3 +1,25 @@
-module.exports = function () {
+module.exports = {
+
+    // Run Function
+    run: function (auth, token) {
+
+        // Sign In
+        auth.signInWithCustomToken(token)
+    
+            // Success
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                // ...
+            })
+    
+            // Fail
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ...
+            });
+    
+    }
 
 };
--- client/logout.js
+++ client/logout.js
@@ -1,3 +1,21 @@
-module.exports = function () {
+module.exports = {
+
+    // Run Function
+    run: function (auth) {
+
+        // Sign Out
+        auth.signOut()
+        
+        // Success
+        .then(() => {
+            // Sign-out successful.
+        })
+        
+        // Fail
+        .catch((error) => {
+            // An error happened.
+        });
+    
+    }
 
 };
