af4a326935fba8d76325818dc6fc229e53a3f412
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-26 19:07:24 -0300

    file new path

--- test/template/cookie-session/firebase/index.js
+++ test/template/cookie-session/firebase/index.js
@@ -1,10 +1,10 @@
 // Prepare Firebase
-const tinyCfg = require('../../firebase_config.json');
+const tinyCfg = require('../../../firebase_config.json');
 const firebase = require('@tinypudding/firebase-lib');
 
 // Get Credentials
 const admin = require('firebase-admin');
-tinyCfg.firebase.credential = admin.credential.cert(require('../../firebase.json'));
+tinyCfg.firebase.credential = admin.credential.cert(require('../../../firebase.json'));
 
 // Start Firebase
 if (tinyCfg.firebase) {
@@ -17,6 +17,6 @@ if (tinyCfg.firebase) {
 const app = firebase.get(tinyCfg.options.id);
 
 // Start Firebase
-require('./index')({ type: 'firebase', app: app, cfg: {
+require('../index')({ type: 'firebase', app: app, cfg: {
     
 } });
