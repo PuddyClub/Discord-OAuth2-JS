572a5cd74334831492a9da002e491bc90fb9eba9
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-08 23:06:48 -0300

    get index

--- get/index.js
+++ get/index.js
@@ -1,10 +1,22 @@
 // Module Base
-const getToken = {
+const getItems = {
 
     // Cookie Session
-    'cookieSession': require('./cookie-session')
+    'cookieSession': require('./cookie-session'),
+
+    // Random Avatar
+    'authURLGenerator': require('./authURLGenerator'),
+
+    // Random Avatar
+    'credentials': require('./credentials'),
+
+    // Random Avatar
+    'errorValidator': require('./errorValidator'),
+
+    // Random Avatar
+    'randomAvatar': require('./randomAvatar')
 
 };
 
 // Send Module
-module.exports = getToken;
+module.exports = getItems;
