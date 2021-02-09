ebd8e5f20d2e94e5aeb8e247258a123cd73dbfb4
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-08 23:08:16 -0300

    firebase redirect index

---
+++ firebase_redirect/index.js
@@ -0,0 +1,13 @@
+// Module Base
+const getItems = {
+
+    // Login
+    'login': require('./login'),
+
+    // Logout
+    'logout': require('./logout')
+
+};
+
+// Send Module
+module.exports = getItems;
